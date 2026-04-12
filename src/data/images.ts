213d25a33392c5fd81215e98e616e69330ea3181
Hemant Gayakwad <hemantgaykwad555@gmail.com>
2026-04-12 23:27:25 +0530

    Update image URLs in images.ts

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,14 +1,14 @@
 // Central image URLs - Change these URLs anytime to update images across the website
 export const IMAGES = {
   logo: "https://ik.imagekit.io/itsprogrammer/channels4_profile.jpg",
-  heroBg: "YOUR_HERO_BG_URL",
-  ceo: "https://ik.imagekit.io/itsprogrammer/Hemant.jpeg",
-  seminar1: "https://ik.imagekit.io/itsprogrammer/seminar%202.jpeg",
+  heroBg: "https://ik.imagekit.io/itsprogrammer/hero.avif",
+  ceo: "https://ik.imagekit.io/itsprogrammer/Hemant%20(2).jpeg",
+  seminar1: "https://ik.imagekit.io/itsprogrammer/seminar%202.jpeg?tr=rt-N91:e-contrast:r-100",
   seminar2: "https://ik.imagekit.io/itsprogrammer/seminar%203.jpeg",
   seminar3: "https://ik.imagekit.io/itsprogrammer/seminar%201.jpeg",
   courseFrontend: "https://ik.imagekit.io/itsprogrammer/Frontend%20course.jpeg",
   courseBackend: "https://ik.imagekit.io/itsprogrammer/Backend%20course.jpeg",
-  courseFullstack: "YOUR_FULLSTACK_COURSE_URL",
-  team1: "https://ik.imagekit.io/itsprogrammer/Akash.jpeg",
+  courseFullstack: "https://ik.imagekit.io/itsprogrammer/Fullstack%20course.jpeg",
+  team1: "https://ik.imagekit.io/itsprogrammer/Akash%20(2).jpeg",
   team2: "https://ik.imagekit.io/itsprogrammer/Vaishnavi.HEIC",
 };
